2225d058a4fdc32a0a7ea8058d801130ef397726
Lee Powell <81479565+leepowe@users.noreply.github.com>
2024-01-01 23:41:40 -0500

    set up nodemon and remember require needs commonjs type

---
+++ 306. Types of Modules/script.js
@@ -0,0 +1,10 @@
+// const c = require('fs');
+// const d = require('http');
+
+// console.log(c);
+const c = require('./script2.js');
+
+const a = c.largeNumber;
+const b = 6;
+
+console.log(a + b);
